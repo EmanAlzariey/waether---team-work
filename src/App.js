a36cacb7ff44ed66e0700b15d8180aebe3f4a75c
TaghreedNageeb <50971460+TaghreedNageeb@users.noreply.github.com>
2019-12-11 17:22:25 +0300

    adding classes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import React, { Component } from 'react';
 import './App.css';
 import ButtonComp from './ButtonComp'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-
 class App extends Component {
   constructor(){
     super()
-
     this.state={
       city:"",
       sanaa:'',
@@ -16,7 +13,6 @@ class App extends Component {
       cairo:''
     }
   }
-
   componentDidMount(){
       fetch(`http://api.weatherstack.com/current?access_key=e49621cf55fd2461357f46f8cc5431e1&query=sanaa}`)
       .then(response => response.json())
@@ -31,7 +27,6 @@ class App extends Component {
       .then(response => response.json())
       .then(json => this.setState({cairo: json}));
   }
-
   fetchsanaa=(e)=>{
    console.log(this.state.sanaa)
    console.log(this.state.sanaa.current.temperature)
@@ -45,19 +40,24 @@ class App extends Component {
   fetchcar=(e)=>{
    console.log(this.state.cairo)
   }
-
-
   render() {
     return (
-      <div>
-        <ButtonComp fetchsanaa={this.fetchsanaa} fetchaden={this.fetchaden} fetchbagh={this.fetchbagh} fetchcar={this.fetchcar} />
+      <div  className= 'container1'>
+        <div className= 'row mt0'>
         
+        
+       
+        <div className ='col-8 col-m-8'>
+        
+        <PicComp temp={json.current.temperature}/>
+        </div>       
+         <div className ='col-8 col-m-8'>
+         <ButtonComp fetchsanaa={this.fetchsanaa} fetchaden={this.fetchaden} fetchbagh={this.fetchbagh} fetchcar={this.fetchcar} />
+         </div>
+         </div>
       </div>
     );
   
   }
 }
-
 export default App;
-
-
