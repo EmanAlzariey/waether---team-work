aadb2449db7ff71e28f1c524b679036a6dd2b999
Firas Muhsen Al-Azizy <firasazizy614@gmail.com>
2019-12-11 15:33:34 +0300

    changed app js from function to class

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 import PicComp from './PicComp'
 import ButtonComp from './ButtonComp'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <PicComp />
-      <ButtonComp />
-    </div>
-  );
+export class App extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    );
+  }
 }
 
 export default App;
+
+
