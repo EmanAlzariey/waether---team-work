61ae2858fad8c9a1e124e6fcd81954ecbddbb0d2
Firas Muhsen Al-Azizy <firasazizy614@gmail.com>
2019-12-11 16:04:56 +0300

    adding functionality to picture comp

--- src/PicComp.js
+++ src/PicComp.js
@@ -1,9 +1,26 @@
 import React from 'react'
 
-
+function chImg(){
+    if(this.props.temp>=33){
+        <img src="../images/summer.jpg"/> 
+    }
+    else if((this.props.temp<33)&&(this.props.temp>=25)){
+        <img src="../images/spring.jpg"/> 
+    }
+    else if((this.props.temp<25)&&(this.props.temp>=10)){
+        <img src="../images/autumn.jpg"/> 
+    }
+    else if(this.props.temp<10){
+        <img src="../images/winter.jpg"/> 
+    }
+           
+}
 
 export default class PicComp extends React.Component{
     render(){
-        return(<div></div>)
+        return(<div>{
+            chImg()
+        }
+        </div>)
     }
 }
