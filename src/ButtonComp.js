ef87953771a854d919cb6c53ef04613294923353
TaghreedNageeb <50971460+TaghreedNageeb@users.noreply.github.com>
2019-12-11 16:50:30 +0300

    finishing fetch

--- src/ButtonComp.js
+++ src/ButtonComp.js
@@ -6,10 +6,10 @@ export default class ButtonComp extends React.Component{
     render(){
         return(
         <div>
-            <button name='sanaa' onClick={this.props.fetchBtn}>Sana'a</button>
-            <button name='aden' onClick={this.props.fetchBtn}>Aden</button>
-            <button name='baghdad' onClick={this.props.fetchBtn}>Baghdad</button>
-            <button name='cairo' onClick={this.props.fetchBtn}>Cairo</button>
+            <button name='sanaa' onClick={this.props.fetchsanaa}>Sana'a</button>
+            <button name='aden' onClick={this.props.fetchaden}>Aden</button>
+            <button name='baghdad' onClick={this.props.fetchbagh}>Baghdad</button>
+            <button name='cairo' onClick={this.props.fetchcar}>Cairo</button>
         </div>
         )
     }
