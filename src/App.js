a9a69074fd76d13318f194774ecedf387b32ed07
Firas Muhsen Al-Azizy <firasazizy614@gmail.com>
2019-12-11 15:40:00 +0300

    adding fetch to app

--- src/App.js
+++ src/App.js
@@ -4,7 +4,26 @@ import PicComp from './PicComp'
 import ButtonComp from './ButtonComp'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
+
 export class App extends Component {
+  constructor(){
+    super();
+
+    state={
+      city:"sanaa"
+    }
+  }
+
+  fetchBtn=()=>{
+    this.setState({
+      city:e.target.name
+    })
+
+    
+    fetch(`http://api.weatherstack.com/current?access_key=e49621cf55fd2461357f46f8cc5431e1&query=${this.state.city}`)
+      .then(response => response.json())
+      .then(data => this.setState({ dataArr: data }));
+  }
   render() {
     return (
       <div>
