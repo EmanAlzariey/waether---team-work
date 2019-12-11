f45c705586ab1709bf5ba397b64548551ee95ade
EmanAlzariey <eman_alzariey@yahoo.com>
2019-12-11 16:18:17 +0300

    fix problems

--- src/App.js
+++ src/App.js
@@ -5,28 +5,29 @@ import ButtonComp from './ButtonComp'
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 
-export class App extends Component {
+class App extends Component {
   constructor(){
-    super();
+    super()
 
-    state={
-      city:""
+    this.state={
+      city:"sana"
     }
   }
 
-  fetchBtn=()=>{
+  fetchBtn=(e)=>{
     this.setState({
       city:e.target.name
     })
-
-    
     fetch(`http://api.weatherstack.com/current?access_key=e49621cf55fd2461357f46f8cc5431e1&query=${this.state.city}`)
       .then(response => response.json())
       .then(data => this.setState({ dataArr: data }));
   }
+
+
   render() {
     return (
       <div>
+        <PicComp />
         <ButtonComp fetchBtn={this.fetchBtn}/>
       </div>
     );
